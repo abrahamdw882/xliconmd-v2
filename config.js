a72c41523168ad889bb56783de92c44efede69e8
ABRAHAM <120536940+abrahamdw882@users.noreply.github.com>
2026-05-09 10:44:46 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
-global.owners = ['25770239992037@lid', '233533763772@s.whatsapp.net'];
+global.owners = ['214302325760156@lid', ''];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
