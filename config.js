bd359fd18862c242b27cf649f9643727fb5fbe06
ABRAHAM <120536940+abrahamdw882@users.noreply.github.com>
2026-05-11 00:59:57 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ global.BOT_PREFIX = '.';
 global.owners = ['214302325760156@lid', ''];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
 global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
+global.ownerName = '𝑨𝑩𝒁𝑻𝑬𝑪𝑯';
