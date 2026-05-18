387933d209378811630a96e646c9381c62cb21b6
Samuel -!Rebix <lordsamueltech@gmail.com>
2026-05-18 18:31:02 +0000

    Update ban-unbanchat.js

--- plugins/ban-unbanchat.js
+++ plugins/ban-unbanchat.js
@@ -3,54 +3,49 @@ module.exports = {
     aliases: ['unbanchat'],
 
     async execute(sock, m, args) {
-
-        if (!global.owners.includes(m.sender)) return;
+        if (!global.owners.includes(m.sender)) return
 
         if (!m.from.endsWith('@g.us')) {
-            return m.reply('ᴛʜɪs ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs');
+            return m.reply('ᴛʜɪs ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs')
         }
 
         if (m.body.startsWith(global.BOT_PREFIX + 'banchat')) {
-
-            if (db.list().groups[m.from].enable === false) {
-                return m.reply('⚠️ ɢʀᴏᴜᴘ ᴀʟʀᴇᴀᴅʏ ʙᴀɴɴᴇᴅ');
+            const groupData = db.get('groups', m.from)
+            if (groupData && groupData.enable === false) {
+                return m.reply('⚠️ ɢʀᴏᴜᴘ ᴀʟʀᴇᴀᴅʏ ʙᴀɴɴᴇᴅ')
             }
 
-            db.editData('groups', m.from, false, 'enable')
-
-            return m.reply('✅ ʙᴏᴛ ʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ');
+            await db.editData('groups', m.from, false, 'enable')
+            return m.reply('✅ ʙᴏᴛ ʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ')
         }
 
         if (m.body.startsWith(global.BOT_PREFIX + 'unbanchat')) {
-
-            if (!global.bannedChats.includes(m.from)) {
-                return m.reply('⚠️ ɢʀᴏᴜᴘ ɪs ɴᴏᴛ ʙᴀɴɴᴇᴅ');
+            const groupData = db.get('groups', m.from)
+            if (!groupData || groupData.enable === true) {
+                return m.reply('⚠️ ɢʀᴏᴜᴘ ɪs ɴᴏᴛ ʙᴀɴɴᴇᴅ')
             }
 
-            global.bannedChats =
-                global.bannedChats.filter(id => id !== m.from);
-
-            return m.reply('✅ ʙᴏᴛ ᴜɴʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ');
+            await db.editData('groups', m.from, true, 'enable')
+            return m.reply('✅ ʙᴏᴛ ᴜɴʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ')
         }
     },
 
     async onMessage(sock, m) {
-
-        if (!global.bannedChats.includes(m.from)) {
-            return false;
+        const groupData = db.get('groups', m.from)
+        
+        if (!groupData || groupData.enable === true) {
+            return false
         }
 
         if (global.owners.includes(m.sender)) {
-            return false;
+            return false
         }
 
-        if (
-            m.body &&
-            m.body.startsWith(global.BOT_PREFIX)
-        ) {
-            return true;
+        if (m.body && m.body.startsWith(global.BOT_PREFIX)) {
+            await m.reply('❌ ʙᴏᴛ ɪs ʙᴀɴɴᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ')
+            return true
         }
 
-        return false;
+        return false
     }
-};
+}
