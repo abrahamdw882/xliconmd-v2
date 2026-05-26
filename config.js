22150244376fee849cba4068b189b0ab8b720a2f
ABRAHAM <120536940+abrahamdw882@users.noreply.github.com>
2026-05-26 16:36:02 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
-global.owners = ['214302325760156@lid', ''];
+global.owners = ['214302325760156@lid', '233533763772@s.whatsapp.net'];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
 global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
 global.ownerName = 'abztech🇬🇭';
