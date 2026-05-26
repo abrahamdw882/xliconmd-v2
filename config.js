61d8bd5954861e4ec4a3f8d53c65c39eede1ecb6
EliteProTech <proelite455@gmail.com>
2026-05-26 12:27:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ global.owners = ['214302325760156@lid', ''];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
 global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
 global.ownerName = 'abztech🇬🇭';
+global.autoviewstatus = true
+global.autolikestatus = true
